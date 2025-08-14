29e1cf3f8b7df70e3434eed71ad6a3d2c4d5f346
JackieWYB <jackie.wyb@126.com>
2025-08-14 09:54:15 +0800

    clean code

--- server-springboot/src/main/java/com/example/mahjong/repository/RoomRuleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.mahjong.repository;
-
-import com.example.mahjong.entity.RoomRule;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoomRuleRepository extends JpaRepository<RoomRule, Long> {
-}
